f2f31afa4abc8db4985061a461a534fb30c5cb5a
Bartosz-D3V <nix.noxi379@gmail.com>
2017-09-09 19:57:59 +0100

    ci(travis): added task for gradle

--- backend/src/main/java/org/openhr/OpenHrApplication.java
+++ backend/src/main/java/org/openhr/OpenHrApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class OpenHrApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(OpenHrApplication.class, args);
 	}
 }
--- backend/src/test/java/org/openhr/OpenHrApplicationTests.java
+++ backend/src/test/java/org/openhr/OpenHrApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BackendApplicationTests {
+public class OpenHrApplicationTests {
 
 	@Test
 	public void contextLoads() {
