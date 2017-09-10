28733dd8611168298f49b0a226da9c7c7c622836
Bartosz-D3V <nix.noxi379@gmail.com>
2017-09-10 17:46:21 +0100

    ci(travis): updated java build tasks

--- backend/src/main/java/org/openhr/BackendApplication.java
+++ backend/src/main/java/org/openhr/BackendApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OpenHrApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OpenHrApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 }
