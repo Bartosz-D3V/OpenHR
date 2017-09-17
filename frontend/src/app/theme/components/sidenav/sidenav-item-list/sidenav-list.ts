1f08492e7688cd42ba40fa5720042dc9a5559cb5
Bartosz-D3V <nix.noxi379@gmail.com>
2017-09-17 21:13:35 +0100

    syles(sidenav): added toggle button and distance between buttons

--- frontend/src/app/theme/components/sidenav/sidenav-item-list/sidenav-list.ts
+++ frontend/src/app/theme/components/sidenav/sidenav-item-list/sidenav-list.ts
@@ -2,7 +2,8 @@ import { SidenavItem } from '../sidenav-item/sidenav-item';
 
 export class SidenavList {
   private _itemList: Array<SidenavItem> = [
-    new SidenavItem('Test', 'star', true)
+    new SidenavItem('Dashboard', 'dashboard', true),
+    new SidenavItem('My leave', 'card_travel', true),
   ];
 
   get itemList(): Array<SidenavItem> {
