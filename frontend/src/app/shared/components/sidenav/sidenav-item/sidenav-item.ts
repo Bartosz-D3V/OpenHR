4c351b630dda6188c03c3e4485c9f02e7b70ceb0
Bartosz-D3V <nix.noxi379@gmail.com>
2017-09-24 14:51:00 +0100

    refactor(sidebar): layout of sidebard updated to match material design

--- frontend/src/app/shared/components/sidenav/sidenav-item/sidenav-item.ts
+++ frontend/src/app/shared/components/sidenav/sidenav-item/sidenav-item.ts
@@ -1,45 +1,13 @@
 export class SidenavItem {
-  private _text: String;
-  private _icon: String;
-  private _link: String;
-  private _isVisible: boolean;
+  public text: String;
+  public icon: String;
+  public link: String;
+  public isVisible: boolean;
 
   constructor(text: String, icon: String, link: String, isVisible: boolean) {
-    this._text = text;
-    this._icon = icon;
-    this._link = link;
-    this._isVisible = isVisible;
-  }
-
-  get text(): String {
-    return this._text;
-  }
-
-  set text(value: String) {
-    this._text = value;
-  }
-
-  get icon(): String {
-    return this._icon;
-  }
-
-  set icon(value: String) {
-    this._icon = value;
-  }
-
-  get link(): String {
-    return this._link;
-  }
-
-  set link(value: String) {
-    this._link = value;
-  }
-
-  get isVisible(): boolean {
-    return this._isVisible;
-  }
-
-  set isVisible(value: boolean) {
-    this._isVisible = value;
+    this.text = text;
+    this.icon = icon;
+    this.link = link;
+    this.isVisible = isVisible;
   }
 }
