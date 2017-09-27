adbd290dcfc73f7ae450df962759575ce548f942
bartosz.wyporkiewicz <nix.noxi379@gmail.com>
2017-09-27 17:03:01 +0100

    test(sidenav): implemented one additional test for ngOnInit listener

--- frontend/src/app/shared/components/sidenav/sidenav.component.spec.ts
+++ frontend/src/app/shared/components/sidenav/sidenav.component.spec.ts
@@ -1,11 +1,14 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
+
+import { MdSidenav, MdSidenavModule } from '@angular/material';
 
 import { SidenavComponent } from './sidenav.component';
 import { SidenavItemComponent } from './sidenav-item/sidenav-item.component';
 import { SidenavItemListComponent } from './sidenav-item-list/sidenav-item-list.component';
-import { MdSidenavModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SidenavComponent', () => {
   let component: SidenavComponent;
@@ -22,7 +25,7 @@ describe('SidenavComponent', () => {
         RouterTestingModule,
         BrowserAnimationsModule,
         MdSidenavModule,
-      ]
+      ],
     })
       .compileComponents();
   }));
@@ -37,15 +40,49 @@ describe('SidenavComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  describe('ngOnInit method', () => {
+    let router: Router;
+    let sidenav: MdSidenav;
+
+    beforeEach(inject([Router], (_router: Router) => {
+      router = _router;
+      sidenav = fixture.debugElement.query(By.directive(MdSidenav)).componentInstance;
+    }));
+
+    it('should call isScreenSmall every time when router emits event', () => {
+      spyOn(component, 'isScreenSmall');
+      fixture.detectChanges();
+
+      expect(component.isScreenSmall).toHaveBeenCalled();
+    });
+
+    // FIX ME
+    xit('should close sidenav if screen is small', fakeAsync(() => {
+      spyOn(component, 'isScreenSmall').and.returnValue(true);
+      spyOn(sidenav, 'close');
+      tick();
+      fixture.detectChanges();
+
+      expect(component.isScreenSmall).toHaveBeenCalled();
+      expect(sidenav.close).toHaveBeenCalled();
+    }));
+
+  });
+
+  // FIX ME
   xdescribe('isScreenSmall method', () => {
 
     it('should return true if max-width is less or equal than 840px', () => {
       spyOn(window, 'matchMedia').and.returnValue({
-        matches: true
-      });
+          matches: true
+        }
+      );
       fixture.detectChanges();
       expect(component.isScreenSmall()).toBeTruthy();
+      expect(component instanceof SidenavComponent).toBeTruthy();
+      expect(component.sidenav.opened).toBeFalsy();
     });
 
   });
+
 });
--- frontend/src/app/shared/components/sidenav/sidenav.component.ts
+++ frontend/src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,16 +1,19 @@
-import { Component, NgZone, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { MdSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class SidenavComponent implements OnInit {
 
-  private mediaMatcher: MediaQueryList = matchMedia(`(max-width: 840px)`);
-  private sidenav: MdSidenav;
+  private mediaMatcher: MediaQueryList = matchMedia('max-width: 840px');
+
+  @ViewChild(MdSidenav)
+  public sidenav: MdSidenav;
 
   constructor(private _router: Router, private zone: NgZone) {
     this.mediaMatcher.addListener(mql => zone.run(() => this.mediaMatcher = mql));
@@ -19,8 +22,7 @@ export class SidenavComponent implements OnInit {
   ngOnInit() {
     this._router.events.subscribe(() => {
       if (this.isScreenSmall()) {
-        this.sidenav.close()
-          .catch((error) => console.error(error));
+        this.sidenav.close();
       }
     });
   }
