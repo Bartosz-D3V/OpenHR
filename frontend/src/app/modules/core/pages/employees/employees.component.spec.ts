35b17e7803b12414890ee8e503a5c7765948bf34
Bartosz-D3V <nix.noxi379@gmail.com>
2017-12-30 21:56:16 +0000

    test: implemented unit tests for employees component

--- frontend/src/app/modules/core/pages/employees/employees.component.spec.ts
+++ frontend/src/app/modules/core/pages/employees/employees.component.spec.ts
@@ -6,6 +6,10 @@ import { MatFormFieldModule, MatInputModule, MatPaginatorModule, MatTableModule 
 import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
 import { CapitalizePipe } from '../../../../shared/pipes/capitalize/capitalize.pipe';
 import { EmployeesComponent } from './employees.component';
+import { Employee } from './domain/employee';
+import { Subject } from '../personal-details/domain/subject';
+import { EmployeeInformation } from '../personal-details/domain/employee-information';
+import { EmployeeData } from './employee-data';
 
 describe('EmployeesComponent', () => {
   let component: EmployeesComponent;
@@ -37,5 +41,27 @@ describe('EmployeesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('applyFilter should lower-case characters and remove white space around string', () => {
+    const mockText1 = '  someTEXT ';
+    component.applyFilter(mockText1);
+    expect(component.dataSource.filter).toEqual('sometext');
+
+    const mockText2 = '1234 Some Text ';
+    component.applyFilter(mockText2);
+    expect(component.dataSource.filter).toEqual('1234 some text');
+  });
+
+  it('simplifyEmployeeObject method should create simplified object from Employee object', () => {
+    const employee: Employee = new Employee(1);
+    let result: EmployeeData;
+    employee.subject = new Subject('John', 'Xavier', null, null, new EmployeeInformation(null, 'Senior Tester', null, null, null));
+    result = component.simplifyEmployeeObject(employee);
+
+    expect(result).toBeDefined();
+    expect(result.id).toBe(1);
+    expect(result.name).toEqual('John Xavier');
+    expect(result.position).toEqual('Senior Tester');
   });
 });
