2cd169190b8f090a0e8dc79ec2c1bf384be2dd5c
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-01-09 15:01:59 +0000

    feat: next phase of auth development

--- backend/src/main/java/org/openhr/configuration/WebSecurity.java
+++ backend/src/main/java/org/openhr/configuration/WebSecurity.java
@@ -1,9 +1,41 @@
 package org.openhr.configuration;
 
+import org.openhr.security.JWTAuthenticationFilter;
+import org.openhr.security.JWTAuthorizationFilter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+  private final UserDetailsService userService;
+  private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+  public WebSecurity(final UserDetailsService userService, final BCryptPasswordEncoder bCryptPasswordEncoder) {
+    this.userService = userService;
+    this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+  }
+
+  @Override
+  public void configure(final HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.csrf().disable().authorizeRequests()
+      .antMatchers("/").permitAll()
+      .antMatchers(HttpMethod.POST, "api/login").permitAll()
+      .antMatchers(HttpMethod.POST, "api/register").permitAll()
+      .anyRequest().authenticated()
+      .and()
+      .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+      .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  }
+
+  @Override
+  protected void configure(final AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+    authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+  }
 }
