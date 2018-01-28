00e9216ae4c1f0b638cdc32f78b18cba8a1dd089
Bartosz-D3V <nix.noxi379@gmail.com>
2018-01-28 17:29:19 +0000

    feat: added missing tests

--- frontend/src/app/shared/components/login-box/login-box.component.spec.ts
+++ frontend/src/app/shared/components/login-box/login-box.component.spec.ts
@@ -1,8 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule } from '@angular/material';
 
 import { LoginBoxComponent } from './login-box.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('LoginBoxComponent', () => {
   let component: LoginBoxComponent;
@@ -11,9 +13,13 @@ describe('LoginBoxComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+        FormsModule,
         MatCardModule,
         MatButtonModule,
         MatIconModule,
+        MatInputModule,
         MatFormFieldModule,
       ],
       declarations: [
@@ -31,5 +37,31 @@ describe('LoginBoxComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Login form', () => {
+    it('username should mark form as invalid if field is empty', () => {
+      component.loginBoxForm.controls['username'].setValue(null);
+
+      expect(component.loginBoxForm.controls['username'].valid).toBeFalsy();
+    });
+
+    it('username should mark form as valid if field is not empty', () => {
+      component.loginBoxForm.controls['username'].setValue('Jack41');
+
+      expect(component.loginBoxForm.controls['username'].valid).toBeTruthy();
+    });
+
+    it('username should mark form as invalid if field is empty', () => {
+      component.loginBoxForm.controls['password'].setValue(null);
+
+      expect(component.loginBoxForm.controls['password'].valid).toBeFalsy();
+    });
+
+    it('username should mark form as valid if field is not empty', () => {
+      component.loginBoxForm.controls['password'].setValue('password123');
+
+      expect(component.loginBoxForm.controls['password'].valid).toBeTruthy();
+    });
   });
 });
