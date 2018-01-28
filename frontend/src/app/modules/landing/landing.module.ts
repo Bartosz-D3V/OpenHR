d5206cc0846630e0b0ffc08ed5db60711938b4bc
Bartosz-D3V <nix.noxi379@gmail.com>
2018-01-28 16:14:20 +0000

    feat: added login page

--- frontend/src/app/modules/landing/landing.module.ts
+++ frontend/src/app/modules/landing/landing.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import 'hammerjs/hammer';
 
 import { LoginComponent } from './pages/login/login.component';
+import { LoginBoxComponent } from '../../shared/components/login-box/login-box.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   declarations: [
     LoginComponent,
+    LoginBoxComponent,
   ],
 })
 export class LandingModule {
