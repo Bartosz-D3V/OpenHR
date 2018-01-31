208b30a7c282e412542d5ccc210a6eb5f55ec5a3
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-01-31 15:03:56 +0000

    feat: added show-hide to buttons based on roles

--- frontend/src/app/shared/components/sidenav/sidenav-item-list/sidenav-item-list.component.spec.ts
+++ frontend/src/app/shared/components/sidenav/sidenav-item-list/sidenav-item-list.component.spec.ts
@@ -1,7 +1,8 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { JwtHelperService } from '../../../services/jwt/jwt-helper.service';
 import { SidenavItemListComponent } from './sidenav-item-list.component';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SidenavItemListComponent', () => {
   let component: SidenavItemListComponent;
@@ -15,6 +16,9 @@ describe('SidenavItemListComponent', () => {
       imports: [
         RouterTestingModule,
       ],
+      providers: [
+        JwtHelperService,
+      ],
     })
       .compileComponents();
   }));
@@ -27,5 +31,12 @@ describe('SidenavItemListComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('hasRole should call the JWT Helper Service', () => {
+    spyOn(component['_jwtHelper'], 'hasRole');
+    component.hasRole('Admin');
+
+    expect(component['_jwtHelper'].hasRole).toHaveBeenCalled();
   });
 });
--- frontend/src/app/shared/components/sidenav/sidenav-item-list/sidenav-item-list.component.ts
+++ frontend/src/app/shared/components/sidenav/sidenav-item-list/sidenav-item-list.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { JwtHelperService } from '../../../services/jwt/jwt-helper.service';
 
 @Component({
   selector: 'app-sidenav-item-list',
   templateUrl: './sidenav-item-list.component.html',
   styleUrls: ['./sidenav-item-list.component.scss'],
+  providers: [JwtHelperService],
 })
 export class SidenavItemListComponent {
+
+  constructor(private _jwtHelper: JwtHelperService) {
+  }
+
+  hasRole(role: string): boolean {
+    return this._jwtHelper.hasRole(role);
+  }
 }
