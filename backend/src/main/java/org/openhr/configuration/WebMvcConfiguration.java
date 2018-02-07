82e61549881bb5aa9b40a48cc49238f2b027e1ea
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-02-07 11:38:26 +0000

    chore: turned off CORS temporarly (set as a variable)

--- backend/src/main/java/org/openhr/configuration/WebMvcConfiguration.java
+++ backend/src/main/java/org/openhr/configuration/WebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package org.openhr.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,12 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class WebMvcConfiguration {
+  @Value("${app.turnOff.cors}")
+  private boolean proxyOff;
+
   @Bean
   public WebMvcConfigurer corsConfigurer() {
     return new WebMvcConfigurerAdapter() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**/*").allowedOrigins("http://localhost:4200");
+        if (!proxyOff) {
+          registry.addMapping("/**/*").allowedOrigins("http://localhost:4200");
+        }
       }
     };
   }
