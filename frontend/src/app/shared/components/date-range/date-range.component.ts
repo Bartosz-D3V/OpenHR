ec1820c353de4b8e3d01b3c1e90cbf967a72b041
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-02-07 15:32:22 +0000

    feat: disabled SSL (temporarly), fixed issue with bank holidays API. Fixed date range picker (#304)

--- frontend/src/app/shared/components/date-range/date-range.component.ts
+++ frontend/src/app/shared/components/date-range/date-range.component.ts
@@ -12,6 +12,7 @@ import * as moment from 'moment';
 import { NAMED_DATE } from '../../../config/datepicker-format';
 import { ResponsiveHelperService } from '../../services/responsive-helper/responsive-helper.service';
 import { ErrorResolverService } from '../../services/error-resolver/error-resolver.service';
+import { JwtHelperService } from '../../services/jwt/jwt-helper.service';
 import { BankHolidayEngland } from './domain/bank-holiday/england/bank-holiday-england';
 import { BankHoliday } from './domain/bank-holiday/england/bank-holiday';
 import { DateRangeService } from './service/date-range.service';
@@ -26,6 +27,7 @@ import { DateRangeService } from './service/date-range.service';
     ResponsiveHelperService,
     DateRangeService,
     ErrorResolverService,
+    JwtHelperService,
   ],
 })
 export class DateRangeComponent implements OnInit, OnDestroy {
@@ -86,10 +88,10 @@ export class DateRangeComponent implements OnInit, OnDestroy {
   public validateStartDateField(): void {
     const startDateCtrl: AbstractControl = this.dateRangeGroup.controls['startDate'];
     this.$startDateChange = startDateCtrl.valueChanges.subscribe((value: Moment) => {
-      if (moment(value).isAfter(this.endDate)) {
+      if (this.endDate && moment(value).isAfter(this.endDate)) {
         startDateCtrl.setErrors({'startDateInvalid': true});
       } else {
-        this.recalculateNumOfDays(this.startDate, this.endDate);
+        this.recalculateNumOfDays(this.startDate, this.endDate, false);
       }
     });
   }
@@ -100,13 +102,13 @@ export class DateRangeComponent implements OnInit, OnDestroy {
       if (this.startDate && moment(value).isBefore(this.startDate)) {
         endDateCtrl.setErrors({'endDateInvalid': true});
       } else {
-        this.recalculateNumOfDays(this.startDate, this.endDate);
+        this.recalculateNumOfDays(this.startDate, this.endDate, false);
       }
     });
   }
 
   public recalculateNumOfDays(startDate: MomentInput, endDate: MomentInput, excludeEndDate?: boolean): void {
-    let diffDays: number = (moment(endDate).diff(startDate, 'days')) + (excludeEndDate ? 0 : +1);
+    let diffDays: number = (moment(endDate).diff(startDate, 'days')) + (excludeEndDate ? -1 : 0) + 1;
     let diffDaysCounter: number = diffDays;
     while (diffDaysCounter > 0) {
       diffDaysCounter--;
