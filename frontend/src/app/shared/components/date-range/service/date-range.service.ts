0534762226dc2d8142f17a83b7c49e2bb93f6873
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-02-07 14:55:08 +0000

    feat: disabled SSL (temporarly), fixed issue with bank holidays API. Fixed date range picker

--- frontend/src/app/shared/components/date-range/service/date-range.service.ts
+++ frontend/src/app/shared/components/date-range/service/date-range.service.ts
@@ -5,23 +5,26 @@ import { Observable } from 'rxjs/Observable';
 
 import { SystemVariables } from '../../../../config/system-variables';
 import { ErrorResolverService } from '../../../services/error-resolver/error-resolver.service';
+import { JwtHelperService } from '../../../services/jwt/jwt-helper.service';
 import { BankHolidayEngland } from '../domain/bank-holiday/england/bank-holiday-england';
 
 @Injectable()
 export class DateRangeService {
-  private url: string = SystemVariables.API_URL + 'bank-holidays/';
+  private url: string = SystemVariables.API_URL + '/bank-holidays';
   private readonly headers: HttpHeaders = new HttpHeaders({
     'Content-Type': 'application/json',
     'Accept': 'application/json',
+    'Authorization': 'Bearer-' + this._jwtHelper.getToken(),
   });
 
-  constructor(private _errorResolverService: ErrorResolverService,
+  constructor(private _jwtHelper: JwtHelperService,
+              private _errorResolverService: ErrorResolverService,
               private _http: HttpClient) {
   }
 
   public getBankHolidaysInEnglandAndWales(): Observable<BankHolidayEngland> {
     return this._http
-      .get<BankHolidayEngland>(this.url + 'england', {
+      .get<BankHolidayEngland>(`${this.url}/england`, {
         headers: this.headers,
       })
       .catch((error: any) => {
