21e464dfb1065999eb0c90ac6d322e7eb61e975e
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-02-09 13:37:56 +0000

    fix: fixed data.sql script being ignored during runtime (#306)

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,8 +1,6 @@
-# INSERT INTO USER_ROLE (USER_ROLE_ID, USER_PERMISSION_ROLE) VALUES ('1', 'ADMIN'), ('2', 'MEMBER');
-# INSERT INTO USER (USER_ID, USERNAME, PASSWORD, USER_ROLE) VALUES ('1', 'bla', 'bla', '1');
-INSERT INTO LEAVE_TYPE (LEAVE_TYPE_ID, TYPE, DESCRIPTION) VALUES
+INSERT INTO LEAVE_TYPE (LEAVE_TYPE_ID, LEAVE_CATEGORY, DESCRIPTION) VALUES
   (1, 'Holiday', 'Paid annual leave'),
   (2, 'Maternity leave', '1 year maternity leave'),
   (3, 'Paternity leave', '1 year paternity leave'),
-  (3, 'Sick day', 'Leave uncounted towards annual leave allowance'),
-  (3, 'Unpaid leave', 'Unpaid leave uncounted towards annual leave allowance')
+  (4, 'Sick day', 'Leave uncounted towards annual leave allowance'),
+  (5, 'Unpaid leave', 'Unpaid leave uncounted towards annual leave allowance');
