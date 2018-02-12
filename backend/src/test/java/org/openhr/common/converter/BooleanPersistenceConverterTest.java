28fb4b4e43827cb070b4a60c0d117a0fe55462a1
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-02-12 16:00:07 +0000

    feat: added converter for boolean values

---
+++ backend/src/test/java/org/openhr/common/converter/BooleanPersistenceConverterTest.java
@@ -0,0 +1,52 @@
+package org.openhr.common.converter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BooleanPersistenceConverterTest {
+  private BooleanPersistenceConverter booleanPersistenceConverter;
+
+  @Before
+  public void setUp() {
+    this.booleanPersistenceConverter = new BooleanPersistenceConverter();
+  }
+
+  @Test
+  public void convertToDatabaseColumnShouldReturnYIfTruePassed() {
+    assertSame('Y', booleanPersistenceConverter.convertToDatabaseColumn(true));
+  }
+
+  @Test
+  public void convertToDatabaseColumnShouldReturnFIfFalsePassed() {
+    assertSame('F', booleanPersistenceConverter.convertToDatabaseColumn(false));
+  }
+
+  @Test
+  public void convertToDatabaseColumnShouldReturnFIfNullPassed() {
+    assertSame('F', booleanPersistenceConverter.convertToDatabaseColumn(null));
+  }
+
+  @Test
+  public void convertToEntityAttributeShouldReturnTrueIfYWasPersisted() {
+    assertTrue(booleanPersistenceConverter.convertToEntityAttribute('Y'));
+  }
+
+  @Test
+  public void convertToEntityAttributeShouldReturnFalseIfFWasPersisted() {
+    assertFalse(booleanPersistenceConverter.convertToEntityAttribute('F'));
+  }
+
+  @Test
+  public void convertToEntityAttributeShouldReturnFalseIfNullWasPersisted() {
+    assertFalse(booleanPersistenceConverter.convertToEntityAttribute(null));
+  }
+}
