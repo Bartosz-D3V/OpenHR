c578a039bba6568477960ca4527d527c8b30e93e
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-02-12 14:55:44 +0000

    fix: fixed test

--- backend/src/test/java/org/openhr/leaveapplication/repository/LeaveApplicationRepositoryTest.java
+++ backend/src/test/java/org/openhr/leaveapplication/repository/LeaveApplicationRepositoryTest.java
@@ -4,13 +4,17 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openhr.application.leaveapplication.domain.LeaveType;
 import org.openhr.application.leaveapplication.repository.LeaveApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -23,7 +27,14 @@ public class LeaveApplicationRepositoryTest {
   private SessionFactory sessionFactory;
 
   @Test
-  public void getLeaveTypesShouldReturnListOfPreDefinedLeaveTypes() {
-    assertEquals(5, leaveApplicationRepository.getLeaveTypes().size());
+  public void getLeaveTypesShouldReturnListOfPreDefinedLeaveTypesAndLeaveTypesAdded() {
+    final LeaveType leaveType1 = new LeaveType("Holiday", "Annual Leave");
+    final LeaveType leaveType2 = new LeaveType("Maternity Leave", "One Year Maternity Leave");
+    final Session session = sessionFactory.getCurrentSession();
+    session.save(leaveType1);
+    session.save(leaveType2);
+    final List<LeaveType> leaveTypes = leaveApplicationRepository.getLeaveTypes();
+
+    assertTrue(leaveTypes.size() >= 2);
   }
 }
