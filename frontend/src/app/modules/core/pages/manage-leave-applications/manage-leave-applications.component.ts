0d6571bb017373b9c7235d0ab7ffbaf9c1d6d830
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-02-14 18:29:47 +0000

    feat: added front-end code for managing leave applications (#332)

--- frontend/src/app/modules/core/pages/manage-leave-applications/manage-leave-applications.component.ts
+++ frontend/src/app/modules/core/pages/manage-leave-applications/manage-leave-applications.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ISubscription } from 'rxjs/Subscription';
+
+import { LeaveApplication } from '../../../../shared/domain/leave-application/leave-application';
+import { JwtHelperService } from '../../../../shared/services/jwt/jwt-helper.service';
+import { ManageLeaveApplicationsService } from './service/manage-leave-applications.service';
 
 @Component({
   selector: 'app-manage-leave-applications',
   templateUrl: './manage-leave-applications.component.html',
   styleUrls: ['./manage-leave-applications.component.scss'],
+  providers: [
+    ManageLeaveApplicationsService,
+    JwtHelperService,
+  ],
 })
-export class ManageLeaveApplicationsComponent implements OnInit {
+export class ManageLeaveApplicationsComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  public leaveApplications: Array<LeaveApplication>;
+  private $leaveApplications: ISubscription;
+
+  constructor(private _manageLeaveApplicationsService: ManageLeaveApplicationsService,
+              private _jwtHelper: JwtHelperService) {
   }
 
   ngOnInit() {
+    this.fetchLeaveApplications();
   }
 
+  ngOnDestroy(): void {
+    this.$leaveApplications.unsubscribe();
+  }
+
+
+  private fetchLeaveApplications(): void {
+    this.$leaveApplications = this._manageLeaveApplicationsService
+      .getUnacceptedLeaveApplications(this._jwtHelper.getSubjectId())
+      .subscribe((result: Array<LeaveApplication>) => this.leaveApplications = result,
+        (error: any) => {
+        });
+  }
 }
