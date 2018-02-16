cc6462738084f74fd84caa64e2a907c1c0a631a9
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-02-16 13:38:13 +0000

    feat: added layout of the new page (#337)

--- frontend/src/app/modules/core/pages/manage-employees-data/manage-employees-data.component.spec.ts
+++ frontend/src/app/modules/core/pages/manage-employees-data/manage-employees-data.component.spec.ts
@@ -1,5 +1,14 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import {
+  MatButtonModule, MatCardModule, MatDatepickerModule, MatExpansionModule, MatFormFieldModule, MatIconModule, MatInputModule,
+  MatNativeDateModule, MatToolbarModule
+} from '@angular/material';
+
+import { PageHeaderComponent } from '../../../../shared/components/page-header/page-header.component';
+import { CapitalizePipe } from '../../../../shared/pipes/capitalize/capitalize.pipe';
 import { ManageEmployeesDataComponent } from './manage-employees-data.component';
 
 describe('ManageEmployeesDataComponent', () => {
@@ -10,9 +19,25 @@ describe('ManageEmployeesDataComponent', () => {
     TestBed.configureTestingModule({
       declarations: [
         ManageEmployeesDataComponent,
+        CapitalizePipe,
+        PageHeaderComponent,
+      ],
+      imports: [
+        NoopAnimationsModule,
+        MatCardModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatToolbarModule,
+        MatExpansionModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatIconModule,
       ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
