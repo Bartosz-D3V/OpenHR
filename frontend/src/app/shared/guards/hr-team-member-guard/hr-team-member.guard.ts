109e02616b6d554da516358843d2d35052a6d472
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-02-28 17:07:02 +0000

    feat: added guards

---
+++ frontend/src/app/shared/guards/hr-team-member-guard/hr-team-member.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+
+import { Role } from '../../domain/subject/role';
+import { JwtHelperService } from '../../services/jwt/jwt-helper.service';
+
+@Injectable()
+export class HrTeamMemberGuard implements CanActivate {
+  constructor(private _jwtHelper: JwtHelperService) {
+  }
+
+  canActivate(): boolean {
+    return !this._jwtHelper.isTokenExpired() &&
+      this._jwtHelper.hasRole(Role.HRTEAMMEMBER);
+  }
+}
