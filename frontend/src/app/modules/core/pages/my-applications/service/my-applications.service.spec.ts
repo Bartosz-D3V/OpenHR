f40c1fedaf80f72a512bd1a20dc9cfa6648e9c3c
Bartosz-D3V <nix.noxi379@gmail.com>
2018-03-04 18:15:20 +0000

    fix: fixed paginator for my applications

--- frontend/src/app/modules/core/pages/my-applications/service/my-applications.service.spec.ts
+++ frontend/src/app/modules/core/pages/my-applications/service/my-applications.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { JwtHelperService } from '../../../../../shared/services/jwt/jwt-helper.service';
 import { MyApplicationsService } from './my-applications.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('MyApplicationsService', () => {
   let service: MyApplicationsService;
