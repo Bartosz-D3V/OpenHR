6b6b00dced52c36c0626c2074b5f0a9fee273c2e
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-03-09 16:16:07 +0000

    feat: method for leave applications status ratio

--- backend/src/main/java/org/openhr/application/dashboard/facade/DashboardFacade.java
+++ backend/src/main/java/org/openhr/application/dashboard/facade/DashboardFacade.java
@@ -1,9 +1,12 @@
 package org.openhr.application.dashboard.facade;
 
 import org.openhr.application.dashboard.dto.MonthSummaryDTO;
+import org.openhr.application.dashboard.dto.StatusRatioDTO;
 
 import java.util.List;
 
 public interface DashboardFacade {
   List<MonthSummaryDTO> getMonthlySummaryReport(long subjectId);
+
+  StatusRatioDTO getCurrentYearStatusRatio();
 }
--- backend/src/main/java/org/openhr/application/dashboard/service/DashboardService.java
+++ backend/src/main/java/org/openhr/application/dashboard/service/DashboardService.java
@@ -1,9 +1,12 @@
 package org.openhr.application.dashboard.service;
 
 import org.openhr.application.dashboard.dto.MonthSummaryDTO;
+import org.openhr.application.dashboard.dto.StatusRatioDTO;
 
 import java.util.List;
 
 public interface DashboardService {
   List<MonthSummaryDTO> getMonthlySummaryReport(long subjectId);
+
+  StatusRatioDTO getCurrentYearStatusRatio();
 }
