61a5b08eac5eb087ef133c778c6c39abcc55e581
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-03-09 17:05:02 +0000

    feat: frontend changes for report display

--- frontend/src/app/modules/core/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/modules/core/pages/dashboard/dashboard.component.ts
@@ -1,69 +1,62 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ISubscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
 
-import { Subject } from '@shared/domain/subject/subject';
+import { DashboardService } from '@modules/core/pages/dashboard/service/dashboard.service';
 import { SubjectDetailsService } from '@shared/services/subject/subject-details.service';
 import { ErrorResolverService } from '@shared/services/error-resolver/error-resolver.service';
-import { HrInformation } from '@shared/domain/subject/hr-information';
-import { SingleChartData } from './domain/single-chart-data';
+import { MonthSummary } from '@modules/core/pages/dashboard/domain/month-summary';
+import { Subject } from '@shared/domain/subject/subject';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   providers: [
+    DashboardService,
     SubjectDetailsService,
   ],
 })
 export class DashboardComponent implements OnInit, OnDestroy {
-  private $subject: ISubscription;
+  private $dashboardService: ISubscription;
+  public monthlySummaries: Array<MonthSummary>;
   public subject: Subject;
-  public allowanceData: Array<SingleChartData> = [];
   public usedAllowance: number;
   public allowanceLeft: number;
 
-  constructor(private _subjectService: SubjectDetailsService,
+  constructor(private _dashboardService: DashboardService,
+              private _subjectService: SubjectDetailsService,
               private _errorResolver: ErrorResolverService) {
   }
 
   ngOnInit() {
-    this.fetchCurrentSubject();
+    this.fetchChartsData();
   }
 
   ngOnDestroy(): void {
-    if (this.$subject !== undefined) {
-      this.$subject.unsubscribe();
+    if (this.$dashboardService !== undefined) {
+      this.$dashboardService.unsubscribe();
     }
   }
 
-  public fetchCurrentSubject(): void {
-    this.$subject = this._subjectService
-      .getCurrentSubject()
-      .subscribe((val: Subject) => {
-        this.subject = val;
-        this.buildCharts(val);
-      }, (err: any) => {
-        this._errorResolver.handleError(err);
-      });
+  public fetchChartsData(): void {
+    this.$dashboardService = Observable.zip(
+      this._subjectService.getCurrentSubject(),
+      this._dashboardService.getMonthlyReport(),
+      (subject: Subject, monthlySummaries: Array<MonthSummary>) => ({subject, monthlySummaries})
+    ).subscribe((pair) => {
+      this.subject = pair.subject;
+      this.buildCharts(pair.subject);
+      this.monthlySummaries = pair.monthlySummaries;
+    }, (err: any) => {
+      this._errorResolver.handleError(err);
+    });
   }
 
   public buildCharts(subject: Subject): void {
     this.allowanceLeft = subject.hrInformation.allowance - subject.hrInformation.usedAllowance;
     this.usedAllowance = this.convertUsedAllowanceToPercent(subject.hrInformation.allowance,
       subject.hrInformation.usedAllowance);
-    this.allowanceData = this.prepareAllowanceData(subject.hrInformation);
-  }
-
-  public prepareAllowanceData(hrInformation: HrInformation): Array<SingleChartData> {
-    const allowanceDataOne: SingleChartData = {
-      'name': 'Total',
-      'value': hrInformation.allowance,
-    };
-    const allowanceDataTwo: SingleChartData = {
-      'name': 'Used',
-      'value': hrInformation.usedAllowance,
-    };
-    return [allowanceDataOne, allowanceDataTwo];
   }
 
   private convertUsedAllowanceToPercent(allowance: number, usedAllowance: number): number {
