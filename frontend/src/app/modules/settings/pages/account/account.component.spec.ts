f95a6ccc2ab77bbc85e7025f29fe1fce40fcd4a7
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-03-20 13:23:22 +0000

    fix: fixed ci issue

--- frontend/src/app/modules/settings/pages/account/account.component.spec.ts
+++ frontend/src/app/modules/settings/pages/account/account.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -20,6 +21,7 @@ describe('AccountComponent', () => {
         CapitalizePipe,
       ],
       imports: [
+        HttpClientTestingModule,
         NoopAnimationsModule,
         FlexLayoutModule,
         FormsModule,
