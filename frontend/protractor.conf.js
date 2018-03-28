07b8ebcb0171a1f8c1011bdfb237c9ecc202a7d5
Bartosz-D3V <nix.noxi379@gmail.com>
2018-03-28 14:22:50 +0200

    chore: increased interval for protractor

--- frontend/protractor.conf.js
+++ frontend/protractor.conf.js
@@ -14,7 +14,7 @@ exports.config = {
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
+    defaultTimeoutInterval: 31000,
     print: function() {},
   },
   onPrepare() {
