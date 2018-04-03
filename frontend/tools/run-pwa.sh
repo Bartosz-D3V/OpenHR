91ed52bc0628b7ceae2b25088120777246e13ad3
bartosz.wyporkiewicz <bartosz.wyporkiewicz@servelec-corelogic.co.uk>
2018-04-03 12:55:29 +0100

    feat: added fallback script to exit dist dir

--- frontend/tools/run-pwa.sh
+++ frontend/tools/run-pwa.sh
@@ -3,7 +3,10 @@ PATH=$PATH:$(npm bin)
 set -x
 
 # Production build
-ng build --prod
+ng build --prod --build-optimizer --aot --no-sourcemap --extract-licenses=false
 
 cd ./dist
 http-server -p 4200
+
+trap cleanup EXIT
+cd ../
