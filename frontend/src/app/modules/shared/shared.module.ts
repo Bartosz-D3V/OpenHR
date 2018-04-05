21b89c90c4cea10055e6155748805ba8a75039eb
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-04-05 09:58:15 +0200

    fix: fixed position of the theme-picker button (#473)

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatGridListModule, MatIconModule, MatMenuModule } from '@angular/material';
+import { MatButtonModule, MatGridListModule, MatIconModule, MatMenuModule } from '@angular/material';
 
 import { PageHeaderComponent } from '@shared/components/page-header/page-header.component';
 import { NumberIndicatorComponent } from '@shared/components/number-indicator/number-indicator.component';
@@ -10,7 +10,16 @@ import { CapitalizePipe } from '@shared/pipes/capitalize/capitalize.pipe';
 import { ThemePickerComponent } from '@shared/components/theme-picker/theme-picker.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, FlexLayoutModule, MatIconModule, MatGridListModule, MatMenuModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatButtonModule,
+  ],
   exports: [CapitalizePipe, PageHeaderComponent, NumberIndicatorComponent, ThemePickerComponent],
   declarations: [CapitalizePipe, PageHeaderComponent, NumberIndicatorComponent, ThemePickerComponent],
 })
