4f760c6f96e5f90ec4666620ae0ad254d2266ec2
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-04-09 20:06:45 +0200

    feat: added mailing for delegation process (#483)

--- backend/src/test/java/org/openhr/processes/delegationapplication/DelegationApplicationProcessTest.java
+++ backend/src/test/java/org/openhr/processes/delegationapplication/DelegationApplicationProcessTest.java
@@ -1,12 +1,12 @@
 package org.openhr.processes.delegationapplication;
 
-import org.activiti.engine.HistoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +15,7 @@ import org.openhr.application.employee.domain.Employee;
 import org.openhr.application.hr.domain.HrTeamMember;
 import org.openhr.application.manager.domain.Manager;
 import org.openhr.application.user.domain.User;
+import org.openhr.application.user.service.UserService;
 import org.openhr.common.domain.subject.ContactInformation;
 import org.openhr.common.domain.subject.EmployeeInformation;
 import org.openhr.common.domain.subject.HrInformation;
@@ -22,10 +23,12 @@ import org.openhr.common.domain.subject.PersonalInformation;
 import org.openhr.common.enumeration.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.subethamail.wiser.Wiser;
 
 import java.time.LocalDate;
 import java.util.HashMap;
@@ -35,6 +38,7 @@ import java.util.UUID;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -43,18 +47,21 @@ import static junit.framework.TestCase.assertTrue;
 public class DelegationApplicationProcessTest {
   private final Employee mockEmployee = new Employee(
     new PersonalInformation("John", "Xavier", "Alex", null),
-    new ContactInformation(), new EmployeeInformation(), new HrInformation(),
+    new ContactInformation("", "j.x@mail.com", null), new EmployeeInformation(), new HrInformation(),
     new User(UUID.randomUUID().toString().substring(0, 19), ""));
   private final Manager mockManager = new Manager(
     new PersonalInformation("John", "Xavier", "Alex", null),
-    new ContactInformation(), new EmployeeInformation(), new HrInformation(),
+    new ContactInformation("", "j.x@mail.com", null), new EmployeeInformation(), new HrInformation(),
     new User(UUID.randomUUID().toString().substring(0, 19), ""));
   private final HrTeamMember mockHrTeamMember = new HrTeamMember(
     new PersonalInformation("John", "Xavier", "Alex", null),
-    new ContactInformation(), new EmployeeInformation(), new HrInformation(),
+    new ContactInformation("", "j.x@mail.com", null), new EmployeeInformation(), new HrInformation(),
     new User(UUID.randomUUID().toString().substring(0, 19), ""));
   private final DelegationApplication mockDelegationApplication = new DelegationApplication(LocalDate.now(),
     LocalDate.now().plusDays(10));
+
+  @MockBean
+  private UserService userService;
 
   @Autowired
   private RuntimeService runtimeService;
@@ -63,10 +70,9 @@ public class DelegationApplicationProcessTest {
   private TaskService taskService;
 
   @Autowired
-  private HistoryService historyService;
-
-  @Autowired
   private SessionFactory sessionFactory;
+
+  private final Wiser wiser = new Wiser();
 
   @Before
   public void setUp() {
@@ -83,6 +89,15 @@ public class DelegationApplicationProcessTest {
     session.save(mockDelegationApplication);
     session.flush();
     session.clear();
+
+    wiser.setHostname("localhost");
+    wiser.setPort(1025);
+    wiser.start();
+  }
+
+  @After
+  public void tearDown() {
+    wiser.stop();
   }
 
   @Test
@@ -162,8 +177,11 @@ public class DelegationApplicationProcessTest {
 
   @Test
   public void afterApplicationIsBeingAcceptedByHRItShouldBeTerminated() {
+    when(userService.notificationsEnabled(mockEmployee.getUser().getUserId())).thenReturn(true);
     Map<String, Object> params = new HashMap<>();
     params.put("subject", mockEmployee);
+    params.put("user", mockEmployee.getUser());
+    params.put("emailAddress", mockEmployee.getContactInformation().getEmail());
     params.put("delegationApplication", mockDelegationApplication);
 
     final ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("delegation-application", params);
@@ -186,6 +204,7 @@ public class DelegationApplicationProcessTest {
     assertTrue(delegationApplication.isApprovedByManager());
     assertTrue(delegationApplication.isApprovedByHR());
     assertTrue(delegationApplication.isTerminated());
+    assertEquals(1, wiser.getMessages().size());
   }
 
   @Test
@@ -230,8 +249,11 @@ public class DelegationApplicationProcessTest {
 
   @Test
   public void processShouldEndImmediatelyIfHRTeamMemberIsApplicant() {
+    when(userService.notificationsEnabled(mockEmployee.getUser().getUserId())).thenReturn(true);
     final Map<String, Object> params = new HashMap<>();
     params.put("subject", mockHrTeamMember);
+    params.put("user", mockEmployee.getUser());
+    params.put("emailAddress", mockEmployee.getContactInformation().getEmail());
     params.put("delegationApplication", mockDelegationApplication);
 
     runtimeService.startProcessInstanceByKey("delegation-application", params);
@@ -242,5 +264,6 @@ public class DelegationApplicationProcessTest {
 
     assertTrue(delegationApplication.isApprovedByHR());
     assertTrue(delegationApplication.isTerminated());
+    assertEquals(1, wiser.getMessages().size());
   }
 }
