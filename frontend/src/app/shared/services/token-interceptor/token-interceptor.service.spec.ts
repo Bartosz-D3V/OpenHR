d472858ae024045bb27c587cf12195c17404e408
Bartosz-D3V <nix.noxi379@gmail.com>
2018-04-20 20:07:25 +0100

    tests: added unit test

--- frontend/src/app/shared/services/token-interceptor/token-interceptor.service.spec.ts
+++ frontend/src/app/shared/services/token-interceptor/token-interceptor.service.spec.ts
@@ -1,19 +1,47 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, fakeAsync } from '@angular/core/testing';
 
 import { TokenInterceptorService } from './token-interceptor.service';
 import { JwtHelperService } from '@shared/services/jwt/jwt-helper.service';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('TokenInterceptorService', () => {
+  let service: TokenInterceptorService;
+  let http: HttpClient;
+  let httpTest: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TokenInterceptorService, JwtHelperService],
+      providers: [
+        TokenInterceptorService,
+        JwtHelperService,
+        HttpClient,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: TokenInterceptorService,
+          multi: true,
+        },
+      ],
+      imports: [HttpClientTestingModule],
     });
+    service = TestBed.get(TokenInterceptorService);
+    http = TestBed.get(HttpClient);
+    httpTest = TestBed.get(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
   });
 
   it(
-    'should be created',
-    inject([TokenInterceptorService], (service: TokenInterceptorService) => {
-      expect(service).toBeTruthy();
+    'should add Bearer token to request',
+    fakeAsync(() => {
+      let response: any;
+      http.get('/api').subscribe((res: any) => {
+        response = res;
+      });
+
+      httpTest.expectOne(req => req.headers.has('Authorization'));
     })
   );
 });
