ef6d0d16433f8ea994308bbe434ec10da5733ee0
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-04-27 19:10:16 +0200

    :rotating_light: tests: added missing frontend tests (#529)

--- frontend/src/app/shared/components/static-modal/static-modal.component.spec.ts
+++ frontend/src/app/shared/components/static-modal/static-modal.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatDialogRef } from '@angular/material';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material';
 import { OverlayContainer } from '@angular/cdk/overlay';
 
 import { StaticModalComponent } from './static-modal.component';
@@ -9,23 +8,31 @@ import { StaticModalComponent } from './static-modal.component';
 describe('StaticModalComponent', () => {
   let component: StaticModalComponent;
   let fixture: ComponentFixture<StaticModalComponent>;
-  let overlayContainerElement: HTMLElement;
+  const overlayContainerElement: HTMLElement = document.createElement('div');
+
+  class FakeMatDialog {
+    public open(): void {}
+  }
+  class FakeMatDialogRef {
+    public close(): void {}
+  }
 
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
         declarations: [StaticModalComponent],
-        imports: [NoopAnimationsModule],
+        imports: [NoopAnimationsModule, MatDialogModule],
         providers: [
           {
             provide: MatDialogRef,
+            useClass: FakeMatDialogRef,
+          },
+          {
+            provide: MatDialog,
+            useClass: FakeMatDialog,
           },
           {
             provide: OverlayContainer,
-            useFactory: () => {
-              overlayContainerElement = document.createElement('div');
-              return { getContainerElement: () => overlayContainerElement };
-            },
           },
         ],
       }).compileComponents();
@@ -42,7 +49,7 @@ describe('StaticModalComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  xit('close method should close modal', () => {
+  it('close method should close modal', () => {
     component.close();
 
     expect(overlayContainerElement.querySelectorAll('mat-dialog-container').length).toBe(0, 'Expected no open dialogs.');
