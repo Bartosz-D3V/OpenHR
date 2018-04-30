118a3b2902bda9499a0d17071af4a1c2112d1df6
bartosz.wyporkiewicz <nix.noxi379@gmail.com>
2018-04-30 17:08:40 +0100

    fix

--- frontend/src/app/shared/services/token-observer/token-observer.service.ts
+++ frontend/src/app/shared/services/token-observer/token-observer.service.ts
@@ -1,30 +1,35 @@
 import { Injectable } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ISubscription } from 'rxjs/Subscription';
 
 import { JwtHelperService } from '@shared/services/jwt/jwt-helper.service';
 import { TokenExpirationModalComponent } from '@shared/components/token-expiration-modal/token-expiration-modal.component';
 
 @Injectable()
 export class TokenObserverService {
-  constructor(private _jwtHelperService: JwtHelperService, public dialog: MatDialog) {}
+  private $jwtHelper: ISubscription;
+
+  constructor(private _jwtHelperService: JwtHelperService, private _dialog: MatDialog) {}
 
   public observe(): void {
     const token: string = this._jwtHelperService.getToken();
-    this._jwtHelperService.startIATObserver(token).subscribe(() => {
-      if (this.dialog.openDialogs.length === 0) {
+    this.$jwtHelper = this._jwtHelperService.startIATObserver(token).subscribe(() => {
+      if (this._dialog.openDialogs.length === 0) {
         this.openDialog();
       }
     });
   }
 
   private openDialog(): void {
-    this.dialog
+    this._dialog
       .open(TokenExpirationModalComponent, {
         width: '250px',
         height: '175px',
         hasBackdrop: true,
         disableClose: true,
+        data: { cancelled: false },
       })
-      .afterClosed(() => this.observe());
+      .afterClosed()
+      .subscribe((data: any) => (!data ? this.observe() : this.$jwtHelper.unsubscribe()));
   }
 }
