0c2ed522ada6d7d1db5fa88af9123727ef6b7cc9
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-05-07 20:29:25 +0200

    finished tests (#553)

--- frontend/e2e/pages/personal-details/personal-details.e2e-spec.ts
+++ frontend/e2e/pages/personal-details/personal-details.e2e-spec.ts
@@ -170,5 +170,230 @@ describe('Personal details page', () => {
           });
       });
     });
+
+    describe('Email input field', () => {
+      it('should contain number field with current user email', () => {
+        expect(PersonalDetailsPo.getEmailField().isPresent()).toBeTruthy();
+        expect(PersonalDetailsPo.getEmailField().getText()).toBe('');
+      });
+
+      it('should display error if email is not correct', () => {
+        PersonalDetailsPo.getEmailField().sendKeys('test@.');
+        PersonalDetailsPo.getTelephoneField()
+          .click()
+          .then(() => {
+            expect(PersonalDetailsPo.getEmailFieldWarningPattern().isPresent()).toBeTruthy();
+            expect(PersonalDetailsPo.getEmailFieldWarningPattern().getText()).toBe('Please enter a valid email address');
+          });
+      });
+
+      it('should display error if email is missing', () => {
+        SharedPo.resetValueByKeyboard(PersonalDetailsPo.getEmailField());
+        PersonalDetailsPo.getTelephoneField()
+          .click()
+          .then(() => {
+            expect(PersonalDetailsPo.getEmailFieldWarningRequired().isPresent()).toBeTruthy();
+            expect(PersonalDetailsPo.getEmailFieldWarningRequired().getText()).toBe('Email is required');
+          });
+      });
+
+      it('should not display error if email is correct', () => {
+        SharedPo.resetValueByKeyboard(PersonalDetailsPo.getEmailField());
+        PersonalDetailsPo.getEmailField().sendKeys('test@e2e-angular.org');
+        PersonalDetailsPo.getTelephoneField()
+          .click()
+          .then(() => {
+            expect(PersonalDetailsPo.getEmailFieldWarningRequired().isPresent()).toBeFalsy();
+            expect(PersonalDetailsPo.getEmailFieldWarningPattern().isPresent()).toBeFalsy();
+          });
+      });
+    });
+
+    describe('address', () => {
+      describe('1st line input field', () => {
+        it('should contain text field with current user 1st line address', () => {
+          expect(PersonalDetailsPo.getFirstLineAddressField().isPresent()).toBeTruthy();
+          expect(PersonalDetailsPo.getFirstLineAddressField().getText()).toBe('');
+        });
+      });
+
+      describe('2nd line input field', () => {
+        it('should contain text field with current user 2nd line address', () => {
+          expect(PersonalDetailsPo.getSecondLineAddressField().isPresent()).toBeTruthy();
+          expect(PersonalDetailsPo.getSecondLineAddressField().getText()).toBe('');
+        });
+      });
+
+      describe('3r line input field', () => {
+        it('should contain text field with current user 3rd line address', () => {
+          expect(PersonalDetailsPo.getThirdLineAddressField().isPresent()).toBeTruthy();
+          expect(PersonalDetailsPo.getThirdLineAddressField().getText()).toBe('');
+        });
+      });
+
+      describe('postcode input field', () => {
+        it('should contain text field with current user postcode', () => {
+          expect(PersonalDetailsPo.getPostcodeField().isPresent()).toBeTruthy();
+          expect(PersonalDetailsPo.getPostcodeField().getText()).toBe('');
+        });
+
+        it('should display error if postcode format is not correct', () => {
+          PersonalDetailsPo.getPostcodeField().sendKeys('12 BG');
+          PersonalDetailsPo.getEmailField()
+            .click()
+            .then(() => {
+              expect(PersonalDetailsPo.getPostcodeFieldWarningPattern().isPresent()).toBeTruthy();
+              expect(PersonalDetailsPo.getPostcodeFieldWarningPattern().getText()).toBe('Please enter a valid postcode');
+            });
+        });
+
+        it('should display error if postcode is missing', () => {
+          SharedPo.resetValueByKeyboard(PersonalDetailsPo.getPostcodeField());
+          PersonalDetailsPo.getEmailField()
+            .click()
+            .then(() => {
+              expect(PersonalDetailsPo.getPostcodeFieldWarningRequired().isPresent()).toBeTruthy();
+              expect(PersonalDetailsPo.getPostcodeFieldWarningRequired().getText()).toBe('Postcode is required');
+            });
+        });
+
+        it('should not display error if postcode format is correct', () => {
+          PersonalDetailsPo.getPostcodeField().sendKeys('SP2 8BE');
+          PersonalDetailsPo.getEmailField()
+            .click()
+            .then(() => {
+              expect(PersonalDetailsPo.getPostcodeFieldWarningPattern().isPresent()).toBeFalsy();
+              expect(PersonalDetailsPo.getPostcodeFieldWarningRequired().isPresent()).toBeFalsy();
+            });
+        });
+      });
+
+      describe('city input field', () => {
+        it('should contain text field with current user country', () => {
+          expect(PersonalDetailsPo.getCityField().isPresent()).toBeTruthy();
+          expect(PersonalDetailsPo.getCityField().getText()).toBe('');
+        });
+      });
+
+      describe('country input field', () => {
+        it('should contain text field with current user country', () => {
+          expect(PersonalDetailsPo.getCountryField().isPresent()).toBeTruthy();
+          expect(PersonalDetailsPo.getCountryField().getText()).toBe('');
+        });
+      });
+    });
+  });
+
+  describe('Employee information', () => {
+    beforeAll(() => {
+      PersonalDetailsPo.getContactInformationNextButton().click();
+    });
+
+    describe('NIN field', () => {
+      it('should contain text field with current user nin', () => {
+        expect(PersonalDetailsPo.getNINField().isPresent()).toBeTruthy();
+        expect(PersonalDetailsPo.getNINField().getText()).toBe('');
+      });
+
+      it('should display error if nin is not correct', () => {
+        PersonalDetailsPo.getNINField().sendKeys('12 HJ 88');
+        PersonalDetailsPo.getEmployeeIDField()
+          .click()
+          .then(() => {
+            expect(PersonalDetailsPo.getNINFieldWarningPattern().isPresent()).toBeTruthy();
+            expect(PersonalDetailsPo.getNINFieldWarningPattern().getText()).toBe('NIN is not correct');
+          });
+      });
+
+      it('should display error if nin is missing', () => {
+        SharedPo.resetValueByKeyboard(PersonalDetailsPo.getNINField());
+        PersonalDetailsPo.getEmployeeIDField()
+          .click()
+          .then(() => {
+            expect(PersonalDetailsPo.getNINFieldWarningRequired().isPresent()).toBeTruthy();
+            expect(PersonalDetailsPo.getNINFieldWarningRequired().getText()).toBe('NIN is required');
+          });
+      });
+
+      it('should not display error if nin is correct', () => {
+        SharedPo.resetValueByKeyboard(PersonalDetailsPo.getNINField());
+        PersonalDetailsPo.getNINField().sendKeys('AX 60 93 31 B');
+        PersonalDetailsPo.getEmployeeIDField()
+          .click()
+          .then(() => {
+            expect(PersonalDetailsPo.getNINFieldWarningRequired().isPresent()).toBeFalsy();
+            expect(PersonalDetailsPo.getNINFieldWarningPattern().isPresent()).toBeFalsy();
+          });
+      });
+    });
+
+    describe('Employee ID', () => {
+      it('should contain text field with current user employee number', () => {
+        expect(PersonalDetailsPo.getEmployeeIDField().isPresent()).toBeTruthy();
+        expect(PersonalDetailsPo.getEmployeeIDField().getText()).toBe('');
+      });
+
+      it('should display error if employee number is missing', () => {
+        SharedPo.resetValueByKeyboard(PersonalDetailsPo.getEmployeeIDField());
+        PersonalDetailsPo.getNINField()
+          .click()
+          .then(() => {
+            expect(PersonalDetailsPo.getEmployeeIDFieldWarningRequired().isPresent()).toBeTruthy();
+            expect(PersonalDetailsPo.getEmployeeIDFieldWarningRequired().getText()).toBe('Employee ID is required');
+          });
+      });
+
+      it('should not display error if employee number is present', () => {
+        SharedPo.resetValueByKeyboard(PersonalDetailsPo.getEmployeeIDField());
+        PersonalDetailsPo.getEmployeeIDField().sendKeys('12X A');
+        PersonalDetailsPo.getNINField()
+          .click()
+          .then(() => {
+            expect(PersonalDetailsPo.getEmployeeIDFieldWarningRequired().isPresent()).toBeFalsy();
+          });
+      });
+    });
+
+    describe('start date input field', () => {
+      it('should contain date field with current user start date', () => {
+        expect(PersonalDetailsPo.getStartDateField().isPresent()).toBeTruthy();
+        expect(PersonalDetailsPo.getStartDateField().getText()).toBe('');
+      });
+    });
+
+    describe('end date input field', () => {
+      it('should contain date field with current user end date', () => {
+        expect(PersonalDetailsPo.getEndDateField().isPresent()).toBeTruthy();
+        expect(PersonalDetailsPo.getEndDateField().getText()).toBe('');
+      });
+    });
+  });
+
+  describe('HR information', () => {
+    beforeAll(() => {
+      PersonalDetailsPo.getEmployeeInformationNextButton();
+    });
+
+    describe('allowance field', () => {
+      it('should contain number field with current user allowance', () => {
+        expect(PersonalDetailsPo.getAllowanceField().isPresent()).toBeTruthy();
+        expect(PersonalDetailsPo.getAllowanceField().getText()).toBe('');
+      });
+
+      it('should not be editable', () => {
+        expect(PersonalDetailsPo.getAllowanceField().isEnabled()).toBeFalsy();
+      });
+    });
+
+    describe('used allowance field', () => {
+      it('should contain number field with current user allowance', () => {
+        expect(PersonalDetailsPo.getUsedAllowanceField().isPresent()).toBeTruthy();
+        expect(PersonalDetailsPo.getUsedAllowanceField().getText()).toBe('');
+      });
+
+      it('should not be editable', () => {
+        expect(PersonalDetailsPo.getUsedAllowanceField().isEnabled()).toBeFalsy();
+      });
+    });
   });
 });
