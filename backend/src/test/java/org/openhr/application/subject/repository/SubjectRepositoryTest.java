d5e235ffc3c2046b9434434e15f2272b9082ef93
Bartosz-D3V <nix.noxi379@gmail.com>
2018-05-20 18:10:22 +0100

    fixed unit test

--- backend/src/test/java/org/openhr/application/subject/repository/SubjectRepositoryTest.java
+++ backend/src/test/java/org/openhr/application/subject/repository/SubjectRepositoryTest.java
@@ -2,10 +2,10 @@ package org.openhr.application.subject.repository;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.UUID;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openhr.application.employee.domain.Employee;
@@ -24,17 +24,17 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest
 @Transactional
 public class SubjectRepositoryTest {
-  private static final Address mockAddress =
+  private final Address mockAddress =
       new Address("100 Fishbury Hs", "1 Ldn Road", null, "12 DSL", "London", "UK");
-  private static final PersonalInformation mockPersonalInformation =
+  private final PersonalInformation mockPersonalInformation =
       new PersonalInformation("John", "Xavier", "Alex", null);
-  private static final ContactInformation mockContactInformation =
+  private final ContactInformation mockContactInformation =
       new ContactInformation("0123456789", "j.x@g.com", mockAddress);
-  private static final EmployeeInformation mockEmployeeInformation =
+  private final EmployeeInformation mockEmployeeInformation =
       new EmployeeInformation("S8821 B", "Tester", "Core", "12A", null, null);
-  private static final HrInformation mockHrInformation = new HrInformation(25L);
-  private static final User mockUser = new User("Jhn13", "testPass");
-  private static final Employee mockSubject =
+  private final HrInformation mockHrInformation = new HrInformation(25L);
+  private final User mockUser = new User(UUID.randomUUID().toString().substring(0, 19), "testPass");
+  private final Employee mockSubject =
       new Employee(
           mockPersonalInformation,
           mockContactInformation,
@@ -46,20 +46,15 @@ public class SubjectRepositoryTest {
 
   @Autowired private SubjectRepository subjectRepository;
 
-  @Before
-  public void setUp() {
-    final Session session = sessionFactory.getCurrentSession();
-    mockHrInformation.setAllowance(25L);
-    mockHrInformation.setHrInformationId(2L);
-    session.saveOrUpdate(mockSubject);
-    session.flush();
-  }
-
   @Test
   public void getAllowanceShouldReturnAllowance() throws HibernateException {
+    final Session session = sessionFactory.getCurrentSession();
+    session.save(mockSubject);
+    session.flush();
+
     final long actualAllowance = subjectRepository.getAllowance(mockSubject.getSubjectId());
 
-    assertEquals(mockHrInformation.getAllowance(), actualAllowance);
+    assertEquals(25L, actualAllowance);
   }
 
   @Test
@@ -68,9 +63,11 @@ public class SubjectRepositoryTest {
     final HrInformation mockHrInformation2 = new HrInformation(20L);
     mockHrInformation2.setUsedAllowance(10L);
     mockSubject.setHrInformation(mockHrInformation2);
-    session.merge(mockSubject);
+    session.save(mockSubject);
+    session.flush();
+
     final long actualUsedAllowance = subjectRepository.getUsedAllowance(mockSubject.getSubjectId());
 
-    assertEquals(mockHrInformation.getUsedAllowance(), actualUsedAllowance);
+    assertEquals(10L, actualUsedAllowance);
   }
 }
