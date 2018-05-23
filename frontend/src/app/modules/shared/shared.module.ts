9caf846700b5aad8ab6f073ae4fba459767e7dfd
bartosz.wyporkiewicz <nix.noxi379@gmail.com>
2018-05-23 13:41:34 +0100

    added validation to the form

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -6,10 +6,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { PageHeaderComponent } from '@shared/components/page-header/page-header.component';
 import { NumberIndicatorComponent } from '@shared/components/number-indicator/number-indicator.component';
 import { CapitalizePipe } from '@shared/pipes/capitalize/capitalize.pipe';
+import { DisableControlDirective } from '@shared/directives/disable-control/disable-control.directive';
+import { ResetControlDirective } from '@shared/directives/reset-control/reset-control.directive';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, FormsModule, FlexLayoutModule],
-  exports: [CapitalizePipe, PageHeaderComponent, NumberIndicatorComponent],
-  declarations: [CapitalizePipe, PageHeaderComponent, NumberIndicatorComponent],
+  exports: [CapitalizePipe, PageHeaderComponent, NumberIndicatorComponent, DisableControlDirective, ResetControlDirective],
+  declarations: [CapitalizePipe, PageHeaderComponent, NumberIndicatorComponent, DisableControlDirective, ResetControlDirective],
 })
 export class SharedModule {}
